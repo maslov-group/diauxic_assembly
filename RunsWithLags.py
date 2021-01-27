dbbb7e8a5c3c23b386a87086cd995d93c255647c
ClaudioMarchisio <wzh41807@gmail.com>
2021-01-27 11:33:44 -0600

    adding the simulation code

---
+++ RunsWithLags.py
@@ -0,0 +1,95 @@
+exec(open("ModulesWithLags.py", encoding = 'utf-8').read())
+exec(open("parameters.py", encoding = 'utf-8').read())
+import numpy as np
+from math import *
+import random
+import itertools
+from scipy.optimize import root
+from scipy import stats
+from scipy import special
+import copy
+import time
+import collections
+import pickle
+import sys
+import multiprocessing
+from multiprocessing import Pool, freeze_support
+
+def model(Nr_in, indices):
+
+    global Size
+    global D
+    global yields_list
+    global growth_rate_list
+    global invlist
+    global details
+    global Res
+    global preference_list
+    global system
+    global movestep
+
+    global g_mu
+    global g_var
+    global scale
+    global theta_low
+    global theta_high_more
+    global lagTime
+    global Nb
+    global Nr
+
+    Nr=Nr_in
+    Nb = factorial(Nr)*Size
+    yields_list = 0.5*np.ones([Nb, Nr])
+    growth_rate_list  = []
+    for i in range(Nb):
+        growth_rate_list.append([max(np.random.normal(g_mu, g_sigma), theta) for i in range(Nr)])
+    invlist = list(range(Nb))
+    random.shuffle(invlist)
+
+    inputFileName = 'input_'+str(indices)+'_lag='+str(lagTime)+'.pkl'
+    pickle.dump({'Nr': Nr, 'G': growth_rate_list, 'invlist': invlist, 'Y': yields_list}, open(inputFileName, "wb" ))
+
+    details = {'res_begin':[], 't_info':[], 'res_left':[], 'bug_info':[], 'round_idx':[0]}
+    Res = [1.0 for i in range(Nr)] # for balanced nutrients
+    # Res = [Nr] + [Nr/100 for i in range(Nr-1)] # for unbalanced nutrients
+    preference_list = list(itertools.permutations(range(Nr), Nr))
+    start=time.time()
+    movestep=0
+    details = {'res_begin':[], 't_info':[], 'res_left':[], 'bug_info':[], 'round_idx':[0]}
+    system = {'res_available': np.heaviside(Res, 0), 'res_concentration': [i for i in Res], 'bug_available': [0 for i in range(Nb)], 'bug_density': [0 for i in range(Nb)]}
+    # ext_list: list of bugs not in the community
+    old_ext_list = invlist
+    #print(old_ext_list)
+    system, new_ext_list = round_robin_invade(system, old_ext_list, growth_rate_list)
+    new_ext_list = [i for i in invlist if i in new_ext_list]
+    count = 0
+    cycled = False
+    while new_ext_list != old_ext_list and count < 10:
+        old_ext_list = new_ext_list
+        survivors, concent, pref_list, growth = output(system)
+        #print(survivors)
+        system, new_ext_list = round_robin_invade(system, old_ext_list, growth_rate_list)
+        count = count + 1
+        if count > 10:
+            cycled = True
+            print('A cycle occurs')
+    #print(survivors)
+    #print(output(system))
+    details['round_idx'] = details['round_idx'][1:]
+    end = time.time()
+    outputFileName = 'outputs_'+str(indices)+'_lag='+str(lagTime)+'.pkl'
+    pickle.dump({'details': getMarketHistory(details), 'output': output(system), 'cycled' : cycled}, open(outputFileName, "wb" ))
+    print(output(system))
+    print(get_experts(growth_rate_list))
+    return (end-start)
+
+if __name__ == '__main__':
+	freeze_support()
+	N_indices = 100
+	cores = multiprocessing.cpu_count()
+	pool = multiprocessing.Pool(processes=9)
+	x1 = [Nr for i in range(N_indices)]
+	x2 = list(range(N_indices))
+	tasks = [(Nr_in, indices) for Nr_in, indices in zip(x1,x2)]
+	print(tasks)
+	pool.starmap(model,tasks)
---
+++ parameters.py
@@ -0,0 +1,15 @@
+Nr = 4
+
+# distribution of growth rates, with symmetric nutrients
+theta = 1e-1 # hard minimum of growth rates
+g_mu  = 0.25
+g_sigma= 0.05
+Size = 480
+lagTime = 0
+# dynamic parameters
+D = 0.01 # (inverse) dilution factor
+T_dilute = 24 # time interval between dilutions
+dilute_to_steady = 10 # #(dilution) between invasions
+b0 = 1e-6 # density of bug when introduced/initial
+b_threshold = 1e-6 # extinction density
+c_threshold = 1e-9 # concentration threshold
